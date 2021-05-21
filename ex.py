a4a1fd2e16977598be9db0e566a37adeedaf631b
super <superpower99x99@gmail.com>
2021-05-21 20:40:01 +0700

    update file ex.py

--- ex.py
+++ ex.py
@@ -52,17 +52,21 @@ def find_df(d, label, data):
       dff = pd.concat([dff, d[d[label] == data[i+1]]])
     return dff
 #Hàm lọc theo search:
-def searchItems(df, data):
-    index = index_documents(load_documents(df), Index())   
+def searchItems(df, index, data):       
     # print(f'Index contains {len(index.documents)} documents')
     search_result = index.search(data, search_type='AND', rank=True)
     dff = df.iloc[search_result]
     return dff
 
-df = pd.read_json('./job_news.json')
+df = pd.read_json('./alljob.json')
 df = df.drop('_id', 1)
-df = df.drop('crawled_time', 1)
-dff = df
+
+with open('./list_types.txt', mode='r', encoding='utf-8') as f:
+  dtype = f.read().split('\n')
+with open('./list_degree.txt', mode='r', encoding='utf-8') as f:
+  ddegree = f.read().split('\n')
+index = index_documents(load_documents(df), Index())
+df1 = df[:][:7000]
 # page layout
 app = dash.Dash(external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 
@@ -150,77 +154,7 @@ app.layout = html.Div([
               dcc.Dropdown(
                   id='types',
                   # Nên sử dụng dạng sau:
-                  # options=[{'label': i, 'value': i} for i in available_indicators],
-                  options=[
-                      {'label': 'Bán hàng', 'value': 'Bán hàng'},
-                      {'label': 'Quản trị kinh doanh', 'value': 'Quản trị kinh doanh'},
-                      {'label': 'Khách sạn - Nhà hàng', 'value': 'Khách sạn - Nhà hàng'},
-                      {'label': 'Marketing - PR', 'value': 'Marketing - PR'},
-                      {'label': 'Xây dựng', 'value': 'Xây dựng'},
-                      {'label': 'Kế toán - Kiểm toán', 'value': 'Kế toán - Kiểm toán'},
-                      {'label': 'Nhân sự', 'value': 'Nhân sự'},
-                      {'label': 'Hành chính - văn phòng', 'value': 'Hành chính - văn phòng'},
-                      {'label': 'KD bất động sản', 'value': 'KD bất động sản'},
-                      {'label': 'An ninh - Bảo vệ', 'value': 'An ninh - Bảo vệ'},
-                      {'label': 'Báo chí - Truyền hình', 'value': 'Báo chí - Truyền hình'},
-                      {'label': 'Bảo hiểm', 'value': 'Bảo hiểm'},
-                      {'label': 'Biên - Phiên dịch', 'value': 'Biên - Phiên dịch'},
-                      {'label': 'Bưu chính', 'value': 'Bưu chính'},
-                      {'label': 'Chứng khoán - vàng', 'value': 'Chứng khoán - vàng'},
-                      {'label': 'Cơ khí - Chế tạo', 'value': 'Cơ khí - Chế tạo'},
-                      {'label': 'Công nghệ cao', 'value': 'Công nghệ cao'},
-                      {'label': 'Công nghiệp', 'value': 'Công nghiệp'},
-                      {'label': 'Dầu khí - Hóa chất', 'value': 'Dầu khí - Hóa chất'},
-                      {'label': 'Dệt may - Da giày', 'value': 'Dệt may - Da giày'},
-                      {'label': 'Dịch vụ', 'value': 'Dịch vụ'},
-                      {'label': 'Du lịch', 'value': 'Du lịch'},
-                      {'label': 'Điện tử viễn thông', 'value': 'Điện tử viễn thông'},
-                      {'label': 'Điện - Điện tử - Điện lạnh', 'value': 'Điện - Điện tử - Điện lạnh'},
-                      {'label': 'Game', 'value': 'Game'},
-                      {'label': 'Giáo dục - Đào tạo', 'value': 'Giáo dục - Đào tạo'},
-                      {'label': 'Hàng gia dụng', 'value': 'Hàng gia dụng'},
-                      {'label': 'Hàng hải', 'value': 'Hàng hải'},
-                      {'label': 'Hàng không', 'value': 'Hàng không'},
-                      {'label': 'Hóa hoc - Sinh học', 'value': 'Hóa hoc - Sinh học'},
-                      {'label': 'Hoạch định dự án', 'value': 'Hoạch định dự án'},
-                      {'label': 'In ấn - Xuất bản', 'value': 'In ấn - Xuất bản'},
-                      {'label': 'IT phần cứng/mạng', 'value': 'IT phần cứng/mạng'},
-                      {'label': 'IT phần mềm', 'value': 'IT phần mềm'},
-                      {'label': 'Kho vận - Vật tư', 'value': 'Kho vận - Vật tư'},
-                      {'label': 'Kiến trúc - TK nội thất', 'value': 'Kiến trúc - TK nội thất'},
-                      {'label': 'Kỹ thuật', 'value': 'Kỹ thuật'},
-                      {'label': 'Kỹ thuật ứng dụng', 'value': 'Kỹ thuật ứng dụng'},
-                      {'label': 'Làm bán thời gian', 'value': 'Làm bán thời gian'},
-                      {'label': 'Lao động phổ thông', 'value': 'Lao động phổ thông'},
-                      {'label': 'Mỹ phẩm - Trang sức', 'value': 'Mỹ phẩm - Trang sức'},
-                      {'label': 'Ngân hàng', 'value': 'Ngân hàng'},
-                      {'label': 'Ngành nghề khác', 'value': 'Ngành nghề khác'},
-                      {'label': 'Nghệ thuật - Điện ảnh', 'value': 'Nghệ thuật - Điện ảnh'},
-                      {'label': 'Ngoại thương - Xuất nhập', 'value': 'Ngoại thương - Xuất nhập'},
-                      {'label': 'Người giúp việc', 'value': 'Người giúp việc'},
-                      {'label': 'Nhân viên kinh doanh', 'value': 'Nhân viên kinh doanh'},
-                      {'label': 'Nông - Lâm - Ngư nghiệp', 'value': 'Nông - Lâm - Ngư nghiệp'},
-                      {'label': 'NV trông quán Internet', 'value': 'NV trông quán Internet'},
-                      {'label': 'Ô tô - Xe máy', 'value': 'Ô tô - Xe máy'},
-                      {'label': 'Pháp lý - Luật', 'value': 'Pháp lý - Luật'},
-                      {'label': 'Promotion Girl', 'value': 'Promotion Girl'},
-                      {'label': 'Quan hệ đối ngoại', 'value': 'Quan hệ đối ngoại'},
-                      {'label': 'Sinh viên làm thêm', 'value': 'Sinh viên làm thêm'},
-                      {'label': 'Tài chính đầu tư', 'value': 'Tài chính đầu tư'},
-                      {'label': 'Thiết kế đồ họa', 'value': 'Thiết kế đồ họa'},
-                      {'label': 'Thiết kế - Mỹ thuật', 'value': 'Thiết kế - Mỹ thuật'},
-                      {'label': 'Thời trang', 'value': 'Thời trang'},
-                      {'label': 'Thủ công mỹ nghệ', 'value': 'Thủ công mỹ nghệ'},
-                      {'label': 'Thư ký - Trợ lý', 'value': 'Thư ký - Trợ lý'},
-                      {'label': 'Thực phẩm - Đồ uống', 'value': 'Thực phẩm - Đồ uống'},
-                      {'label': 'Thực tập', 'value': 'Thực tập'},
-                      {'label': 'Thương mại điện tử', 'value': 'Thương mại điện tử'},
-                      {'label': 'Tiếp thị - Quảng cáo', 'value': 'Tiếp thị - Quảng cáo'},
-                      {'label': 'Tổ chức sự kiện - Quà tặng', 'value': 'Tổ chức sự kiện - Quà tặng'},
-                      {'label': 'Tư vấn', 'value': 'Tư vấn'},
-                      {'label': 'Vận tải lái xe', 'value': 'Vận tải lái xe'},
-                      {'label': 'Y tế - Dược', 'value': 'Y tế - Dược'},
-                  ],
+                  options=[{'label': i, 'value': i} for i in dtype],
                   value = "Y tế - Dược",
                   multi=True
               ),
@@ -248,16 +182,8 @@ app.layout = html.Div([
               dcc.RadioItems(
                 # chưa đưa ra được chính xác option về bằng cấp
                   id='degree', 
-                  options=[
-                      {'label': 'Không yêu cầu', 'value': 'Không yêu cầu'},
-                      {'label': u'Trung học', 'value': 'Trung học'},
-                      {'label': 'Trung cấp', 'value': 'Trung cấp'},
-                      {'label': 'Cao đẳng', 'value': 'Cao đẳng'},
-                      {'label': 'Đại học', 'value': 'Đại học'},
-                      {'label': 'Trên đại học', 'value': 'Trên đại học'}
-
-                  ],
-                  value='Trung học',
+                  options=[{'label': i, 'value': i} for i in ddegree],
+                  value='Không yêu cầu',
                   labelStyle={'display': 'block'}
               ),
 
@@ -300,9 +226,9 @@ app.layout = html.Div([
           style_data={
             'whiteSpace': 'normal',
           },
-          data=dff.to_dict('records'),
+          data=df1.to_dict('records'),
           columns=[
-              {"name": i, "id": i,} for i in dff.columns
+              {"name": i, "id": i,} for i in df1.columns
           ],
           css=[{
               'selector': '.dash-spreadsheet td div',
@@ -317,7 +243,7 @@ app.layout = html.Div([
               {
                   column: {'value': str(value), 'type': 'markdown'}
                   for column, value in row.items()
-              } for row in dff.to_dict('records')
+              } for row in df1.to_dict('records')
           ],
           tooltip_duration=None,
 
@@ -327,17 +253,13 @@ app.layout = html.Div([
             'textOverflow': 'ellipsis',
             'whiteSpace': 'normal',
             'minWidth': '80px', 'width': '80px', 'maxWidth': '80px',
-            }, # left align text in columns for readability
-          # style_table={'height': 1000},
-          
+            }, 
           editable=True,
           filter_action="native",
           sort_action="native",
           sort_mode="multi",
           column_selectable="single",
           row_selectable="multi",
-#           selected_columns=[],
-#           selected_rows=[],
           page_action="native",
           page_current= 0,
           page_size= 10,
@@ -352,42 +274,31 @@ app.layout = html.Div([
     Output(component_id='datatable-interactivity', component_property='tooltip_data'),
     Input(component_id='search', component_property='value'),
     Input(component_id='location', component_property='value'),
-    # Input(component_id='types', component_property='value'),
-    # Input(component_id='degree', component_property='value'),
+    Input(component_id='types', component_property='value'),
+    Input(component_id='degree', component_property='value'),
     # Input(component_id='experience', component_property='value'),
-    # Input(component_id='gender', component_property='value'),
+    Input(component_id='gender', component_property='value'),
 )
 
 
 # def update_data(search_, location_, types_, degree_, experience_, gender_):
-def update_data(search_, location_):
-    # search trả về value
-    # if location_ != [] and types_ != []:
-    #   dff = find_df(df, 'working_location', location_)
-    #   dff = find_df(df, 'types', types_)
-    #   # dff = find_df(df, 'degree', degree_)
-    #   # dff = find_df(df, 'experience', experience_)
-    #   # dff = find_df(df, 'gender', gender_)
-    # elif types_ != []:
-    #   dff = find_df(df, 'types', types_)
-      # dff = find_df(df, 'degree', degree_)
-      # dff = find_df(df, 'experience', experience_)
-      # dff = find_df(df, 'gender', gender_)
+def update_data(search_, location_,types_,degree_, gender_):
     dff = df
-    
+    if search_ != '':
+      dff = searchItems(dff, index, search_)
     if location_ != []:
       dff = find_df(dff, 'working_location', location_)
-      # dff = find_df(df, 'degree', degree_)
-      # dff = find_df(df, 'experience', experience_)
-      # dff = find_df(df, 'gender', gender_)
-    if search_ != '':
-      dff = searchItems(dff, search_)
-    data = dff.to_dict('records')
+    if types_ != []:
+      dff = find_df(dff, 'types', types_)
+    dff = dff[dff['gender'] == gender_]
+    dff = dff[dff['degree'] == degree_]
+    
+    data = dff[:][:7000].to_dict('records')
     tooltip_data=[
         {
             column: {'value': str(value), 'type': 'markdown'}
             for column, value in row.items()
-        } for row in dff.to_dict('records')
+        } for row in dff[:][:7000].to_dict('records')
     ]
     return data, tooltip_data
 
